10b8336bd9f8781fc971a2b75f835ca576bd69d4
SerhiyT <s.serhiy.t@gmail.com>
2019-08-13 20:31:52 +0300

    Message components CSS_2

--- src/components/Message/Message.index.js
+++ src/components/Message/Message.index.js
@@ -2,24 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
 import enLocale from 'date-fns/locale/en';
+import classNames from 'classnames';
+
+import svgReaded from '../../assets/icons/Readed.svg';
+import svgUnReaded from '../../assets/icons/UnReaded.svg';
 
 import '../Message/Message.scss';
 
 
-const Message = ({ avatar, user, text, date }) => {
+const Message = ({ avatar, user, text, date, isMe, isReaded }) => {
     return (
-        <section className="message">
-            <div className="message__avatar">
-                <img src={avatar} alt={`Avatar ${user.fullname}`} />
-            </div>
+        <div className={classNames("message", { "message--isme": isMe })}>
             <div className="message__content">
-                <div className="message__bubble">
-                    <p className="message__text">{text}</p>
+                {isMe && isReaded ? (
+                    <img
+                        src={svgReaded}
+                        alt="Cheked icon"
+                        className="message__iconCheked" />
+                ) : (
+                        <img
+                            src={svgUnReaded}
+                            alt="UnCheked icon"
+                            className="message__iconCheked" />
+                    )}
+                <div className="message__avatar">
+                    <img src={avatar} alt={`Avatar ${user.fullname}`} />
                 </div>
-                <time className="message__date">{distanceInWordsToNow(date, { addSuffix: true, locale: enLocale })}</time>
+                <div className="message__info">
+                    <div className="message__bubble">
+                        <p className="message__text">{text}</p>
+                    </div>
+                    <time className="message__date">
+                        {distanceInWordsToNow(date, { addSuffix: true, locale: enLocale })}
+                    </time>
+                </div>
             </div>
-        </section>
-
+        </div>
     );
 }
 
--- src/pages/Home/Home.index.jsx
+++ src/pages/Home/Home.index.jsx
@@ -12,14 +12,22 @@ const Home = () => (
                 name="Serh"
                 text="Text messaging, or texting, is the act of composing and sending electronic"
                 date="Mon Aug 12 2019 15:23:02" />
-                <Message avatar="https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-1/c0.1.160.160a/p160x160/65880009_2343537199033014_3578762992377921536_n.jpg?_nc_cat=100&_nc_oc=AQnoCz5V3pWZ2327C3zIfdUFmgS9LJU44P4cFxnoEkw1f0Gfbmn3e-qmtWoRN6_OePg&_nc_ht=scontent-waw1-1.xx&oh=5b9521f558a8b8833c02ebf8bb8c4004&oe=5DDEBE47"
+            <Message avatar="https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/996022_394140564047516_799882837_n.jpg?_nc_cat=102&_nc_oc=AQmti9-2-RGlIbc1pUZcPZDVD5mwtnbE29l1asnf1Tmzm06R1_B9C9DCuwPVdVEvYHU&_nc_ht=scontent-waw1-1.xx&oh=f5385b46db92a7e20e00e05fade5b4e2&oe=5DE62E0B"
+                name="Serh"
+                text="Text messaging, or texting, is the act of composing and sending electronic"
+                date="Mon Aug 12 2019 19:45:14"
+                isMe={true}
+                isReaded={true} />
+            <Message avatar="https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-1/c0.1.160.160a/p160x160/65880009_2343537199033014_3578762992377921536_n.jpg?_nc_cat=100&_nc_oc=AQnoCz5V3pWZ2327C3zIfdUFmgS9LJU44P4cFxnoEkw1f0Gfbmn3e-qmtWoRN6_OePg&_nc_ht=scontent-waw1-1.xx&oh=5b9521f558a8b8833c02ebf8bb8c4004&oe=5DDEBE47"
                 name="Serh"
                 text="Text messaging, or texting, is the act of composing and sending electronic"
                 date="Mon Aug 12 2019 15:23:02" />
-                <Message avatar="https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-1/c0.1.160.160a/p160x160/65880009_2343537199033014_3578762992377921536_n.jpg?_nc_cat=100&_nc_oc=AQnoCz5V3pWZ2327C3zIfdUFmgS9LJU44P4cFxnoEkw1f0Gfbmn3e-qmtWoRN6_OePg&_nc_ht=scontent-waw1-1.xx&oh=5b9521f558a8b8833c02ebf8bb8c4004&oe=5DDEBE47"
+            <Message avatar="https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/996022_394140564047516_799882837_n.jpg?_nc_cat=102&_nc_oc=AQmti9-2-RGlIbc1pUZcPZDVD5mwtnbE29l1asnf1Tmzm06R1_B9C9DCuwPVdVEvYHU&_nc_ht=scontent-waw1-1.xx&oh=f5385b46db92a7e20e00e05fade5b4e2&oe=5DE62E0B"
                 name="Serh"
                 text="Text messaging, or texting, is the act of composing and sending electronic"
-                date="Mon Aug 12 2019 15:23:02" />
+                date="Mon Aug 12 2019 19:45:14"
+                isMe={true}
+                isReaded={false} />
         </div>
     </section>
 )
